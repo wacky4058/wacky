0050a710116fe4ed00762a70624ad4f3bc54f9be
wacky4058 <wacky4058@163.com>
2020-02-29 15:41:33 +0800

    firest

---
+++ src/main/java/com/shao/wacky/WackyApplication.java
@@ -0,0 +1,13 @@
+package com.shao.wacky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WackyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WackyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/shao/wacky/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.shao.wacky.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "hello world!";
+    }
+}
